b5f33eefea76ed24ca8dc9db30ef4c13b715e61f
Hemant Mooneea <hemantmooneea1@gmail.com>
2024-07-18 13:36:28 +0400

    can write to db

---
+++ Backend/firebase.js
@@ -0,0 +1,33 @@
+import admin from 'firebase-admin'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+class firebase
+{
+    constructor()
+    {
+        admin.initializeApp({
+            credential:admin.credential.cert({
+                projectId:process.env.FIREBASE_PROJECT_ID,
+                clientEmail:process.env.FIREBASE_CLIENT_EMAIL,
+                privateKey:process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+            }),
+            databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.asia-southeast1.firebaseio.com`
+        });
+        
+        this.db = admin.firestore();
+    }
+    async testConnection()
+    {
+        try {
+            const ref = this.db.collection('test').doc('data');
+            await ref.set({ message: 'Hello from Firebase!' });
+            console.log('Write successful!');
+          } catch (error) {
+            console.error('Write failed:', error);
+          }
+    }
+}
+
+export default firebase;
