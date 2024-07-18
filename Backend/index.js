e947d217aaf45da6733e45c38b03dc704a369d57
Hemant Mooneea <hemantmooneea1@gmail.com>
2024-07-18 13:36:47 +0400

    can call firebase db

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,24 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import firebase from './firebase.js';
 
 const app = express();
 const PORT = 3000;
+const db = new firebase();
 
 app.use(bodyParser.json());
 
+
+(async () => {
+    try {
+      await db.testConnection();
+    } catch (error) {
+      console.error("Error connecting to Firebase:", error);
+    }
+  })();
+
 app.get('/', (req, res) => {
+    db.testConnection();
     res.send('Oweiss Gunga');
 });
 
