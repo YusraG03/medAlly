a7809f66e3a7eb0a1c94e93b7d3a68700e9b1a4b
Hemant Mooneea <hemantmooneea1@gmail.com>
2024-07-18 11:13:53 +0400

    add boilerplate express code

---
+++ Backend/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Oweiss Gunga');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
