335691ed9268cf7df2ca77defb4902476290309f
oweissg <oweissgunga@gmail.com>
2024-07-25 14:37:53 +0400

    Created a testing hub and a basic folder structure

--- Frontend/medally-app/app/index.js
+++ Frontend/medally-app/app/index.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!helloooo</Text>
-      <Text>Welcome to MedAlly!</Text>
+      <View style = {styles.header}>
+      <Text style = {styles.h1}>Medally Testing HQ</Text>
+      <Text style = {styles.posttitle} >Select your path.</Text>
+      </View>
+      <Link style = {styles.button} href="./initialtesting">About</Link>
       <StatusBar style="auto" />
     </View>
   );
@@ -20,7 +24,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   }, 
-  text:{
-    fontSize : 20,
+  h1: {
+    fontSize : 30,
+  },
+  posttitle: {
+    fontSize : 22,
+    color: '#4F4F4F',
+  },
+  button:{
+    color: '#F7F7F7',
+    backgroundColor : '#121419',
+    paddingHorizontal:20,
+    paddingVertical:10,
+    fontSize : 18,
+  },
+  header:{
+    flexDirection:'column',
+    padding:50,
+    alignItems: 'center',
   }
 });
