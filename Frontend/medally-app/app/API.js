0087924ff5dc5597c162a73cf42ee7a137a9b77c
Hemant Mooneea <hemantmooneea1@gmail.com>
2024-08-02 11:06:30 +0400

    can now get all medication of a user as well as add new medication

--- Frontend/medally-app/app/API.js
+++ Frontend/medally-app/app/API.js
@@ -1,20 +1,29 @@
 // APIEndpoint.js
-export default class APIEndpoint {
-    constructor() {
-        this.url = "http://localhost:3000"; // Replace with your actual URL
+class APIEndpoint 
+{
+    constructor()
+    {
+        this.url = "http://localhost:3000";
     }
-    async testConnection() {
-        try {
+    async testConnection() 
+    {
+        try 
+        {
             const response = await fetch(this.url);
             const data = await response.text();
             console.log('data');
-        } catch (error) {
+        } 
+        catch (error) 
+        {
             console.error('Error connecting to API:', error);
         }
     }
-    async registerUser(userCreds) {
-        try {
-            const response = await fetch(`${this.url}/register`, {
+    async registerUser(userCreds) 
+    {
+        try 
+        {
+            const response = await fetch(`${this.url}/register`, 
+            {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -23,13 +32,18 @@ export default class APIEndpoint {
             });
             const data = await response.json();
             console.log(data);
-        } catch (error) {
+        } 
+        catch (error) 
+        {
             console.error('Error registering user:', error);
         }
     }
-    async loginUser(userCreds) {
-        try {
-            const response = await fetch(`${this.url}/login`, {
+    async loginUser(userCreds) 
+    {
+        try 
+        {
+            const response = await fetch(`${this.url}/login`, 
+            {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -38,8 +52,53 @@ export default class APIEndpoint {
             });
             const data = await response.json();
             console.log(data);
-        } catch (error) {
+        } catch (error) 
+        {
             console.error('Error logging in user:', error);
         }
     }
+    async addMedication(medication, userCreds)
+    {
+        try 
+        {
+            const response = await fetch(`${this.url}/addMedication`, 
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ medication, userCreds }),
+            });
+            const data = await response.json();
+            console.log(data);
+        } 
+        catch (error) 
+        {
+            console.error('Error storing medication:', error);
+        }
+    }
+    async getAllMedication(userCreds) 
+    {
+        try 
+        {
+            const response = await fetch(`${this.url}/getAllMedication`, 
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ userCreds }),
+            });
+            const data = await response.json();
+            console.log(data);
+        } 
+        catch (error) 
+        {
+            console.error('Error getting medication:', error);
+        }
+    }
 }
+const test = new APIEndpoint();
+test.addMedication({"medicationName":"portlouis"},{"email":"example@gmail.com"});
+
+//export default APIEndpoint;   
