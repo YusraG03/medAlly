cce01b930c5a86a6fa7b313092ccfd0886f6bd93
OubaidM <maudarbux.oubaid@gmail.com>
2024-08-05 23:01:40 +0400

    Change export method in userStorage.js

--- Frontend/medally-app/app/account/userStorage.js
+++ Frontend/medally-app/app/account/userStorage.js
@@ -2,18 +2,13 @@ import fs from 'fs';
 
 const userFilePath = "./user-id.txt";
 
-function saveUserId(userId) {
+export function saveUserId(userId) {
     fs.writeFileSync(userFilePath, userId, 'utf8');
 }
 
-function getUserId() {
+export function getUserId() {
     if (fs.existsSync(userFilePath)) {
         return fs.readFileSync(userFilePath, 'utf8');
     }
     return null;
 }
-
-module.exports = {
-    saveUserId,
-    getUserId
-};
