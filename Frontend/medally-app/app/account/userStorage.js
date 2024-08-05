dcb8e72f47b062ab983d1a772f6a00d965760d8a
OubaidM <maudarbux.oubaid@gmail.com>
2024-08-05 23:34:11 +0400

    userStorage.js new trial

--- Frontend/medally-app/app/account/userStorage.js
+++ Frontend/medally-app/app/account/userStorage.js
@@ -1,35 +1,19 @@
-import { promises as fs } from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import fs from 'fs/promises';
+import { existsSync } from 'fs';
 
-// Define __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const userFilePath = './user_id.txt';
 
-const userFilePath = path.join(__dirname, 'user-id.txt');
-
-class SaveUser {
+class UserStorage {
     async saveUserId(userId) {
-        try {
-            await fs.writeFile(userFilePath, userId, 'utf8');
-        } catch (error) {
-            console.error('Error saving user ID:', error);
-        }
+        await fs.writeFile(userFilePath, userId, 'utf8');
     }
     
     async getUserId() {
-        try {
-            await fs.access(userFilePath);
-            const data = await fs.readFile(userFilePath, 'utf8');
-            return data;
-        } catch (error) {
-            if (error.code === 'ENOENT') {
-                return null; // File does not exist
-            }
-            console.error('Error reading user ID:', error);
+        if (existsSync(userFilePath)) {
+            return await fs.readFile(userFilePath, 'utf8');
         }
         return null;
     }
 }
 
-export default SaveUser;
+export default UserStorage;
