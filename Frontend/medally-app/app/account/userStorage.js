86fefe0a06be4a7fa0e9b3b93593343885282875
OubaidM <maudarbux.oubaid@gmail.com>
2024-08-05 23:42:04 +0400

    another userStorage trial

--- Frontend/medally-app/app/account/userStorage.js
+++ Frontend/medally-app/app/account/userStorage.js
@@ -1,23 +1,33 @@
 import fs from 'fs';
-import { promisify } from 'util';
-
-const writeFile = promisify(fs.writeFile);
-const readFile = promisify(fs.readFile);
-const fileExists = promisify(fs.exists);
 
 const userFilePath = './user_id.txt';
 
 class UserStorage {
     async saveUserId(userId) {
-        await writeFile(userFilePath, userId, 'utf8');
+        return new Promise((resolve, reject) => {
+            fs.writeFile(userFilePath, userId, 'utf8', (err) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
     }
     
     async getUserId() {
-        const exists = await fileExists(userFilePath);
-        if (exists) {
-            return await readFile(userFilePath, 'utf8');
-        }
-        return null;
+        return new Promise((resolve, reject) => {
+            fs.access(userFilePath, fs.constants.F_OK, (err) => {
+                if (err) {
+                    return resolve(null);
+                }
+                fs.readFile(userFilePath, 'utf8', (err, data) => {
+                    if (err) {
+                        return reject(err);
+                    }
+                    resolve(data);
+                });
+            });
+        });
     }
 }
 
