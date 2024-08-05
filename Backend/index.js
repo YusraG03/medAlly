b48f447786baaef8f47e9b351b75e65caf12e885
OubaidM <maudarbux.oubaid@gmail.com>
2024-08-05 15:35:01 +0400

    Server is now hosted on public IP

--- Backend/index.js
+++ Backend/index.js
@@ -196,7 +196,7 @@ app.post('/upload', upload.single('image'), async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running at http://0.0.0.0:${PORT}/`);
 });
 
