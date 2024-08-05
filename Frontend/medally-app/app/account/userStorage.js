847ab0dbf5acf1bb3afce6538cb329b3f169bba3
Hemant Mooneea <hemantmooneea1@gmail.com>
2024-08-05 23:27:23 +0400

    fix PLS MO ENVI DORMI

--- Frontend/medally-app/app/account/userStorage.js
+++ Frontend/medally-app/app/account/userStorage.js
@@ -1,5 +1,10 @@
 import { promises as fs } from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Define __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const userFilePath = path.join(__dirname, 'user-id.txt');
 
