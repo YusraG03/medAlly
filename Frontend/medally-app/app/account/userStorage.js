03ee06860d90a16f6fb4d7ef20a347290bd52524
Hemant Mooneea <hemantmooneea1@gmail.com>
2024-08-06 09:30:17 +0400

    add web userID storage

--- Frontend/medally-app/app/account/userStorage.js
+++ Frontend/medally-app/app/account/userStorage.js
@@ -1,3 +1,4 @@
+import { Platform } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const userFilePath = 'user_id';
@@ -5,17 +6,27 @@ const userFilePath = 'user_id';
 class UserStorage {
     async saveUserId(userId) {
         try {
-            await AsyncStorage.setItem(userFilePath, userId);
-            console.log(`User ID ${userId} saved successfully.`);
+            if (Platform.OS === 'web') {
+                localStorage.setItem(userFilePath, userId);
+                console.log(`User ID ${userId} saved successfully in localStorage.`);
+            } else {
+                await AsyncStorage.setItem(userFilePath, userId);
+                console.log(`User ID ${userId} saved successfully in AsyncStorage.`);
+            }
         } catch (error) {
             console.error('Error saving user ID:', error);
         }
-    }   
-    
+    }
+
     async getUserId() {
         try {
-            const userId = await AsyncStorage.getItem(userFilePath);
-            return userId !== null ? userId : null;
+            if (Platform.OS === 'web') {
+                const userId = localStorage.getItem(userFilePath);
+                return userId !== null ? userId : null;
+            } else {
+                const userId = await AsyncStorage.getItem(userFilePath);
+                return userId !== null ? userId : null;
+            }
         } catch (error) {
             console.error('Error retrieving user ID:', error);
             return null;
