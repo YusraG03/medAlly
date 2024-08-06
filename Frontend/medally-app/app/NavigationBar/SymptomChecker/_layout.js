cd2562822d0967d24e5111906808f04ff34e31a7
oweissg <oweissgunga@gmail.com>
2024-08-06 21:41:04 +0400

    various tweaks & fixes

--- Frontend/medally-app/app/NavigationBar/SymptomChecker/_layout.js
+++ Frontend/medally-app/app/NavigationBar/SymptomChecker/_layout.js
@@ -3,8 +3,14 @@ import { Stack } from 'expo-router';
 export default function Layout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: 'Home' }} />
-      <Stack.Screen name="results" options={{ title: 'Results' }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen 
+        name="results" 
+        options={{ 
+          title: 'Results', 
+          headerBackTitle: 'Back'  // or 'Home' if you prefer
+        }} 
+      />
     </Stack>
   );
 }
